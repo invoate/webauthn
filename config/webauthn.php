14d2f1042eaf3014decb7d02b81e511c0cb98727
Oliver Lumby <me@oliverlumby.com>
2023-03-04 18:49:07 +0100

    wip

--- config/webauthn.php
+++ config/webauthn.php
@@ -9,18 +9,6 @@ return [
     ],
 
     'registration' => [
-        // can be "discouraged", "preferred" or "required"
-        'resident-key' => 'preferred',
-
-        // can be "discouraged", "preferred" or "required"
-        'user-verification' => 'preferred',
-
-        // allowed transports "usb", "nfc", "ble", and "internal"
-        'exclude-credentials' => ['usb', 'nfc', 'ble', 'internal'],
-
-        // allowed formats "apple", "packed", "tpm", "android-key", "android-safetynet", "fido-u2f" and "none"
-        'formats' => ['apple', 'packed', 'tpm', 'android-key', 'android-safetynet', 'fido-u2f', 'none'],
-
         'session-key' => 'webauthn',
     ],
 
